a0a1b4c44a880fec2c66a6428eead93e1d17aea2
Aron Richter <aron.richter@contabilizei.com.br>
2020-09-19 19:19:40 -0300

    adicionado comunicação com db

---
+++ config/database.js
@@ -0,0 +1,16 @@
+const { Pool } = require('pg');
+
+const pool = new Pool({
+  connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+pool.on('connect', () => {
+  console.log('Base de Dados conectado com sucesso!');
+});
+
+module.exports = {
+  query: (text, params) => pool.query(text, params),
+};
--- dao/repositorio.dao.js
+++ dao/repositorio.dao.js
@@ -1,53 +1,46 @@
+const db = require("../config/database.js");
+
 module.exports = {
   async create(item) {
-    console.log("Criado: ", item);
+    await db.query(
+      `INSERT INTO repositorio (nome,
+        proprietario,
+        descricao,
+        url,
+        linguagem,
+        forks,
+        issues,
+        estrelas,
+        seguidores,
+        privado) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10)`,
+      [item.id, 
+        item.name,
+        item.owner.login,
+        item.html_url,
+        toUpperCase(item.language),
+        item.forks_count,
+        item.open_issues_count,
+        item.stargazers_count,
+        item.private]
+    );
     return true;
   },
 
   async delete(linguagem) {
     console.log("Excluído: ", linguagem);
+    await db.query('DELETE FROM repositorio WHERE linguagem = $1', [
+      toUpperCase(linguagem)
+    ]);
+  
     return true;
   },
 
   async read(linguagem) {
     console.log("Encontrado: ", linguagem)
-    return [
-      {
-        nome: "Teste 01",
-        proprietario: "Aron",
-        descricao: "Teste 01",
-        url: "https://github.com/Aron/Teste-01",
-        linguagem: "Java",
-        forks: 3,
-        issues: 2,
-        estrelas: 32,
-        seguidores: 12,
-        privado: false,
-      },
-      {
-        nome: "Teste 02",
-        proprietario: "ARichter",
-        descricao: ":Teste 02",
-        url: "https://github.com/ARichter/Teste-02",
-        linguagem: "Java",
-        forks: 2,
-        issues: 4,
-        estrelas: 12,
-        seguidores: 7,
-        privado: false,
-      },
-      {
-        nome: "Teste 03",
-        proprietario: "ARich",
-        descricao: "Teste 03",
-        url: "https://github.com/ARich/Teste-03",
-        linguagem: "Java",
-        forks: 5,
-        issues: 8,
-        estrelas: 86,
-        seguidores: 8,
-        privado: false,
-      }
-    ]
+    const response = await db.query('SELECT * FROM repositorio WHERE linguagem = $1', [
+      toUpperCase(linguagem)
+    ]);
+    
+    return response.rows;
   }
 }
